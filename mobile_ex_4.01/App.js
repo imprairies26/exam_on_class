5f31883d80571717aac701f034eb7799acbafea8
KenAto <164752557+imprairies26@users.noreply.github.com>
2026-03-07 10:06:12 +0700

    Update App.js

--- mobile_ex_4.01/App.js
+++ mobile_ex_4.01/App.js
@@ -1,58 +1,28 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet, Alert, Platform } from 'react-native';
-import LoginInput from './components/LoginInput';
+import {StyleSheet, Button, View, Text, Alert, Platform} from 'react-native';
 
-const App = () => {
-  
-  const handleValidate = (phoneNumber) => {
-    const phoneRegex = /^(0[3|5|7|8|9])([0-9]{8})$/;
-    if (phoneNumber.trim() === '') {
-      if(Platform.OS === "web") {
-        window.alert('Thông báo', 'Vui lòng không để trống số điện thoại')
-      } else {
-      Alert.alert('Thông báo', 'Vui lòng không để trống số điện thoại');
-      }
-    } else if (phoneRegex.test(phoneNumber)) {
-      if(Platform.OS === "web") {
-        window.alert('Thành công', 'Số điện thoại hợp lệ!');
-      } else {
-        Alert.alert('Thành công', 'Số điện thoại hợp lệ!');
-      }
-    } else {
-      if(Platform.OS === "web") {
-        window.alert('Lỗi định dạng', 'Số điện thoại không đúng định dạng. Vui lòng kiểm tra lại');
-      } else {
-        Alert.alert('Lỗi định dạng', 'Số điện thoại không đúng định dạng. Vui lòng kiểm tra lại');
-      }
-    } 
-  };
+function showAlert(message) {
+  if (Platform.OS === 'web') {
+    //window.alert(message);
+  } else {
+    Alert.alert(message);
+  }
+}
 
+export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Đăng nhập</Text>
-      </View>
-      <View style={styles.line} />
+    <View style={styles.container}>
+      <Button onPress={showAlert("hello")} title="Say"/>
 
-      <LoginInput onValidate={handleValidate} />
-      
-    </SafeAreaView>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  header: {
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-  },
-  headerText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  line: {
-    height: 1,
-    backgroundColor: '#f0f0f0',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
 });
-
-export default App;
