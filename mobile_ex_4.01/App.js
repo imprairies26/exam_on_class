0dc0ff09657e2f03e9167e1d43da3d025b7e122c
KenAto <164752557+imprairies26@users.noreply.github.com>
2026-03-07 10:03:25 +0700

    Refactor App component and add phone validation

--- mobile_ex_4.01/App.js
+++ mobile_ex_4.01/App.js
@@ -1,28 +1,58 @@
 import React from 'react';
-import {StyleSheet, Button, View, Text, Alert, Platform} from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, Alert, Platform } from 'react-native';
+import LoginInput from './components/LoginInput';
 
-function showAlert(message) {
-  if (Platform.OS === 'web') {
-    //window.alert(message);
-  } else {
-    Alert.alert(message);
-  }
-}
+const App = () => {
+  
+  const handleValidate = (phoneNumber) => {
+    const phoneRegex = /^(0[3|5|7|8|9])([0-9]{8})$/;
+    if (phoneNumber.trim() === '') {
+      if(Platform.OS === "web") {
+        window.alert('Thông báo', 'Vui lòng không để trống số điện thoại')
+      } else {
+      Alert.alert('Thông báo', 'Vui lòng không để trống số điện thoại');
+      }
+    } else if (phoneRegex.test(phoneNumber)) {
+      if(Platform.OS === "web") {
+        window.alert('Thành công', 'Số điện thoại hợp lệ!');
+      } else {
+        Alert.alert('Thành công', 'Số điện thoại hợp lệ!');
+      }
+    } else {
+      if(Platform.OS === "web") {
+        window.alert('Lỗi định dạng', 'Số điện thoại không đúng định dạng. Vui lòng kiểm tra lại');
+      } else {
+        Alert.alert('Lỗi định dạng', 'Số điện thoại không đúng định dạng. Vui lòng kiểm tra lại');
+      }
+    } 
+  };
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      <Button onPress={showAlert("hello")} title="Say"/>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Đăng nhập</Text>
+      </View>
+      <View style={styles.line} />
 
-    </View>
+      <LoginInput onValidate={handleValidate} />
+      
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+  header: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  line: {
+    height: 1,
+    backgroundColor: '#f0f0f0',
   },
 });
+
+export default App;
