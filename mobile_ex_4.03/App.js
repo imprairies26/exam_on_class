4538dd09251ed470b9ce03b785a2318f1cd573dd
KenAto <164752557+imprairies26@users.noreply.github.com>
2026-03-07 10:06:49 +0700

    Add phone number validation in LoginInput

--- mobile_ex_4.03/App.js
+++ mobile_ex_4.03/App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
-import LoginInput from './components/LoginInput'; // Nhớ kiểm tra đường dẫn file này nhé!
+import { SafeAreaView, View, Text, StyleSheet, Alert, Platform } from 'react-native';
+import LoginInput from './components/LoginInput';
 
 const App = () => {
+  
+  const handleValidate = (phoneNumber) => {
+    const phoneRegex = /^(0[3|5|7|8|9])([0-9]{8})$/;
+    if (phoneNumber.trim() === '') {
+      if(Platform.OS === "web") {
+        window.alert('Thông báo', 'Vui lòng không để trống số điện thoại')
+      } else {
+      Alert.alert('Thông báo', 'Vui lòng không để trống số điện thoại');
+      }
+    } else if (phoneRegex.test(phoneNumber)) {
+      if(Platform.OS === "web") {
+        window.alert('Thành công', 'Số điện thoại hợp lệ!');
+      } else {
+        Alert.alert('Thành công', 'Số điện thoại hợp lệ!');
+      }
+    } else {
+      if(Platform.OS === "web") {
+        window.alert('Lỗi định dạng', 'Số điện thoại không đúng định dạng. Vui lòng kiểm tra lại');
+      } else {
+        Alert.alert('Lỗi định dạng', 'Số điện thoại không đúng định dạng. Vui lòng kiểm tra lại');
+      }
+    } 
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-     
       <View style={styles.header}>
         <Text style={styles.headerText}>Đăng nhập</Text>
       </View>
       <View style={styles.line} />
 
-      <LoginInput />
+      <LoginInput onValidate={handleValidate} />
       
     </SafeAreaView>
   );
